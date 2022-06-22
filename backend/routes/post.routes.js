b111be32c372f7fe5f0859fd738c85baea4f37b1
Andy Cordon <acordon@predell.fr>
2022-06-22 16:17:14 +0200

    Like et Commentaire (erreur non def via postman)

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -8,11 +8,11 @@ router.post("/", postController.createPost); //créer un post
 router.put("/:id", postController.updatePost); //modifier un post
 router.delete("/:id", postController.deletePost); //supprimer un post
 router.patch("/like-post/:id", postController.likePost); //like un post
-// router.patch("/unlike-post/:id", postController.unlikePost); //Ne pas liker un post
+router.patch("/unlike-post/:id", postController.unlikePost); //unlike un post
 
 // comments
-// router.patch("/comment-post/:id", postController.commentPost);
-// router.patch("/edit-comment-post/:id", postController.editCommentPost);
-// router.patch("/delete-comment-post/:id", postController.deleteCommentPost);
+router.patch("/comment-post/:id", postController.commentPost); //commenter un post
+router.patch("/edit-comment-post/:id", postController.editCommentPost); //modifier un commentaire de post
+router.patch("/delete-comment-post/:id", postController.deleteCommentPost); //supprimer un commentaire de post
 
 module.exports = router;
