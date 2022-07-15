dec3fd87d48a601bf16751d406471aea12d916d8
Andy Cordon <acordon@predell.fr>
2022-07-15 09:54:17 +0200

    Configuration axios ok

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import Routes from "./components/Routes";
+import { UidContext } from "./components/AppContext";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { getUser } from "./actions/user.actions";
+
+const App = () => {
+  const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios("http://localhost:5000/jwtid", { withCredentials: true })
+      .then((res) => setUid(res.data))
+      .catch((err) => console.log(err));
+
+    if (uid) dispatch(getUser(uid));
+  }, [uid]);
+
+  return (
+    <UidContext.Provider value={uid}>
+      <Routes />
+    </UidContext.Provider>
+  );
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import rootReducer from "./reducers";
+import { getUsers } from "./actions/users.actions";
+// dev tools
+
+import { getPosts } from "./actions/post.actions";
+
+const store = configureStore({ reducer: rootReducer });
+// rootReducer,
+// composeWithDevTools(applyMiddleware(thunk))
+
+store.dispatch(getUsers());
+store.dispatch(getPosts());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
