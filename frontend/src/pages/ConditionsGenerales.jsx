e4c4eb7846f4221c45a654b09da01fe54cf764a2
Andy Cordon <acordon@predell.fr>
2022-07-25 14:30:44 +0200

    creation et css conditions page

--- frontend/src/pages/ConditionsGenerales.jsx
+++ frontend/src/pages/ConditionsGenerales.jsx
@@ -1,5 +1,89 @@
 import React from "react";
 
 export default function ConditionsGénérales() {
-  return <div>Voici la liste des condition générales</div>;
+  return (
+    <div>
+      <section>
+        <h1>Conditions générales d'utilisation</h1>
+        <article>
+          <h2>Partie 1</h2>
+          <h3>Article 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <h3>Article 2</h3>
+          <p>
+            Blandit libero volutpat sed cras ornare arcu dui vivamus arcu. Est
+            sit amet facilisis magna etiam tempor orci eu lobortis. Augue
+            interdum velit euismod in pellentesque massa placerat. Pellentesque
+            eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+            Tincidunt ornare massa eget egestas purus viverra accumsan in nisl.{" "}
+          </p>
+          <h3>Article 3</h3>
+          <p className="last-p">
+            Tortor condimentum lacinia quis vel eros donec ac odio. Orci
+            phasellus egestas tellus rutrum tellus pellentesque eu tincidunt
+            tortor.{" "}
+          </p>
+        </article>
+        <article>
+          <h2>Partie 2</h2>
+          <h3>Article 3</h3>
+          <p>
+            Et magnis dis parturient montes nascetur ridiculus mus. Facilisis
+            volutpat est velit egestas. Aliquet enim tortor at auctor urna nunc
+            id cursus. Vestibulum lorem sed risus ultricies tristique. Integer
+            malesuada nunc vel risus.{" "}
+          </p>
+          <h3>Article 1</h3>
+          <p>
+            Faucibus et molestie ac feugiat sed lectus. Facilisis mauris sit
+            amet massa vitae tortor condimentum lacinia quis. Molestie ac
+            feugiat sed lectus vestibulum mattis ullamcorper velit sed. Sit amet
+            est placerat in egestas erat. Luctus venenatis lectus magna
+            fringilla urna porttitor rhoncus dolor purus.
+          </p>
+          <h3>Article 2</h3>
+          <p>
+            Interdum velit laoreet id donec ultrices tincidunt arcu non sodales.
+            Facilisis magna etiam tempor orci eu lobortis elementum. Id donec
+            ultrices tincidunt arcu non sodales neque.
+          </p>
+          <h3>Article 3</h3>
+          <p className="last-p">
+            Nisl vel pretium lectus quam id leo. Dolor sed viverra ipsum nunc
+            aliquet bibendum.
+          </p>
+        </article>
+        <article>
+          <h2>Partie 3</h2>
+          <h3>Article 1</h3>
+          <p>
+            Pellentesque eu tincidunt tortor aliquam nulla facilisi cras
+            fermentum odio. Porttitor lacus luctus accumsan tortor posuere ac.
+            Aenean euismod elementum nisi quis eleifend quam adipiscing vitae
+            proin. Turpis massa sed elementum tempus egestas. Dui accumsan sit
+            amet nulla facilisi morbi tempus. Facilisi etiam dignissim diam quis
+            enim.
+          </p>
+          <h3>Article 2</h3>
+          <p className="last-p">
+            Nec tincidunt praesent semper feugiat. Duis at tellus at urna
+            condimentum mattis pellentesque id nibh. In iaculis nunc sed augue
+            lacus viverra vitae. Id aliquet lectus proin nibh nisl condimentum
+            id venenatis a. Nulla porttitor massa id neque aliquam vestibulum.
+            Massa massa ultricies mi quis. Dictum at tempor commodo ullamcorper
+            a lacus. Lectus urna duis convallis convallis tellus id interdum.
+            Elit scelerisque mauris pellentesque pulvinar.
+          </p>
+        </article>
+      </section>
+    </div>
+  );
 }
