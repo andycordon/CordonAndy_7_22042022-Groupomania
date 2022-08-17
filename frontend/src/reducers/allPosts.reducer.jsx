911318c679b721f2edd6a478e63c35f2993dbd85
Andy Cordon <acordon@predell.fr>
2022-08-17 15:15:33 +0200

    suppression fichier devenu obsolete et format jsx

--- frontend/src/reducers/allPosts.reducer.jsx
+++ frontend/src/reducers/allPosts.reducer.jsx
@@ -5,8 +5,8 @@ const initialState = {};
 export default function allPostsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_ALL_POSTS:
-      return action.payload
-    default: 
+      return action.payload;
+    default:
       return state;
   }
 }
--- frontend/src/reducers/error.reducer.jsx
+++ frontend/src/reducers/error.reducer.jsx
@@ -1,21 +1,21 @@
 import { GET_POST_ERRORS } from "../actions/post.actions";
 import { GET_USER_ERRORS } from "../actions/user.actions";
 
-const initialState = { userError: [], postError: []};
+const initialState = { userError: [], postError: [] };
 
 export default function errorReducer(state = initialState, action) {
   switch (action.type) {
     case GET_POST_ERRORS:
       return {
         postError: action.payload,
-        userError: []
-      }
+        userError: [],
+      };
     case GET_USER_ERRORS:
       return {
         userError: action.payload,
-        postError: []
-      }
-    default: 
+        postError: [],
+      };
+    default:
       return state;
   }
 }
